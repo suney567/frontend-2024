631c982dd499f9e6f20ceee69d0b3ce83027432d
Suneyana Das <suneyana8@gmail.com>
2024-12-04 15:50:37 +0530

    frontend

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from"react";
+import "./Header.css";
+
+export default function Header(){
+    return(
+        <div className="header-title header-font">
+            <h1>My React App</h1>
+        </div>
+    );
+}
